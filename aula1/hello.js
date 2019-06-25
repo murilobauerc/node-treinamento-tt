a17088f71141ce3d12476008864035a480927608
aluno02 sala01 <aluno02sala01@targettrust.local>
2019-06-24 22:16:04 -0300

    using express

--- aula1/hello.js
+++ aula1/hello.js
@@ -1,8 +1,9 @@
 const { sum, sub, division,multiply } = require('./calculadora')
+const upperCase = require('upper-case')
 
 console.log(`1 + 2 = ${sum(1,2)}`);
 console.log(`4 - 2 = ${sub(4,2)}`);
 console.log(`5 / 1 = ${division(5,1)}`);
 console.log(`2 * 2 = ${multiply(2,2)}`);
 
-console.log('Hello,world')
+console.log(upperCase('Hello,world'))
---
+++ demo/index.js
@@ -0,0 +1,5 @@
+const surprise = require('surprise')
+
+console.log('demo package', surprise());
+
+
